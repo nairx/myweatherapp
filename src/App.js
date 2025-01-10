f02163b49a1b7d1a5d9b443d26edf825f2727d70
nairx <nairxprv@gmail.com>
2025-01-10 14:02:46 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import { useState, useEffect } from "react";
 function App() {
   const [city, setCity] = useState("Bangalore");
-  const [data, setData] = useState(null);
+  const [flag,setFlag] = useState(false)
+  const [data, setData] = useState({});
   const apikey = process.env.REACT_APP_APIKEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
   const showData = () => {
@@ -10,18 +11,29 @@ function App() {
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch((err) => console.log(err));
+
+    if (data.cod===200) setFlag(true) 
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>My Weather App</h1>
-        <div><input type="text" placeholder="Enter City Name" onChange={(e) => setCity(e.target.value)}></input>{" "}
-        <button onClick={showData}>Fetch</button></div>
-        {data && (
+        <div>
+          <input
+            type="text"
+            placeholder="Enter City Name"
+            onChange={(e) => setCity(e.target.value)}
+          ></input>{" "}
+          <button onClick={showData}>Fetch</button>
+        </div>
+
+        {flag && (
           <div>
-            <div>{data.name}, {data.sys.country} </div>
-            <div>Temperature: {Math.round(data.main.temp-273.15)} °C</div>
+            <div>
+              {data.name}, {data.sys.country}{" "}
+            </div>
+            <div>Temperature: {Math.round(data.main.temp - 273.15)} °C</div>
             <div>{data.weather[0].description}</div>
           </div>
         )}
